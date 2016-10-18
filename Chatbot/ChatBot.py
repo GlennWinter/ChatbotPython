747bdce39f489265995b91c196d0d1d6c201ad1f
Glenn Winter <winter.glenn@hotmail.co.uk>
2016-10-18 17:57:49 +0100

    First submission

---
+++ Chatbot/ChatBot.py
@@ -0,0 +1,12 @@
+f = open("stopwords.txt")
+lines = f.readlines()
+
+print("Hello")
+greeting = raw_input()
+name = raw_input("What is your name?")
+print("Hello " + name)
+age = input("How old are you?")
+year = (2016 - age)
+
+
+print (int("You were born in " + year))

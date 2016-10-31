aa6db35dceae93807d204dd631931d974fc53e8f
Glenn Winter <winter.glenn@hotmail.co.uk>
2016-10-31 05:21:28 +0000

    Finished Chatbot

--- Chatbot/ChatBot.py
+++ Chatbot/ChatBot.py
@@ -1,48 +1,76 @@
-import random
-import wikipedia
-import time
+import random, wikipedia, time
+
 
 stopWords = open("stopwords.txt", "r+")
+stopwords = stopWords.read()
+stopWords.close()
+
 happyWords = open("happyWords.txt", "r+")
+happywords = happyWords.read()
+happyWords.close()
+
 sadWords = open("sadWords.txt", "r+")
-#print stopWords
-#happyWords.write("good")
-#happyWords.close()
-
-
+sadwords = sadWords.read()
+sadWords.close()
 
 welcomePhrases = ("Hey", "Yo", "Hello", "Hi", "Welcome", "Bonjour")
- 
-
+happyPhrases = ("That's good to hear!", "Sounds great!", "Glad you're good", "That's grand")
+sadPhrases = ("That's not good!", "Hope you're okay!", "That sucks to hear!", "Sorry to hear that")
+fairwellPhrases = ("Goodbye", "Cya", "Laters", "Catch you soon", "Bye!", "Fairwell")
 
 def findKeyword(words):
     for word in words:
-        if word.lower() not in stopWords: return word
+        if word.lower() not in stopwords: return word
+        
+def findHappy(happyWord):
+    for word in happyWord:
+        if word.lower() in happywords: 
+            print(random.choice(happyPhrases))
+        
+def findSad(sadWord):
+    for word in sadWord:
+        if word.lower() in sadwords: 
+            print(random.choice(sadPhrases))
     
 def wikiSearch():
     
-    print "Did you mean " + research + ", " + wikipedia.summary(research, sentences=2) + "\n"
+    print ("Did you mean " + research + ", " + wikipedia.summary(research, sentences=2) + "\n")
     
 while True: 
       
     print("Anyone there?")
-    response = raw_input("> ")
-
+    response = raw_input(">")
 
     print(random.choice(welcomePhrases))
-    response = raw_input("> ")
+    response = raw_input(">")
     
-    name = raw_input("What is your name?\n > ")
+    name = raw_input("What is your name?\n>")
     
     splitWords = name.split(" ")
     keyWord = findKeyword(splitWords)
     print(random.choice(welcomePhrases) + " " + keyWord)
+    
     time.sleep(1)
     print("I am a chatbot called...")
     time.sleep(2)
     print("Chatbot")
     time.sleep(1)   
-    age = input("How old are you " + name + "?\n >")
+    
+    feeling = raw_input("How are you feeling today " + keyWord + "?\n>")
+    
+    splitWords = feeling.split(" ")
+    feelingResponse = [findHappy(splitWords), findSad(splitWords)]
+    
+    if (findHappy in feelingResponse is True):
+            findHappy()
+            
+    elif (findSad in feelingResponse is True):
+            findSad()
+            
+    else: 
+        print("I do not understand that feeling")
+
+    age = input("How old are you " + keyWord + "?\n>")
     year = 2016 - age
     time.sleep(1)
     print("Ah so you were born in " + str(year))
@@ -61,26 +89,28 @@ while True:
     
     time.sleep(2)
     print("Is there anything you would like to learn about? ")
-    research = raw_input()
-    print(" ")      
+    time.sleep(1)
+    research = raw_input("Please enter what you want to search \n>")
+         
     wikiSearch() 
     
-    while True: 
-        userInput = raw_input("Is there anything else you would like to learn about?\n >")
+    while True:
+        print("Is there anything else you would like to learn about?")
+        userInput = raw_input()
            
             
         if (userInput == "yes"):
-            print("Please enter what you want to search\n >")
-            research = raw_input()
+            print("Please enter what you want to search")
+            research = raw_input(">")
             wikiSearch()
         elif(userInput == "no"):
             break 
         
         else:
-            print("Please answer with yes or no  >")
+            print("Please answer with yes or no")
             
         
-    favColour = raw_input("Okay then, what is your favourite colour?\n >")
+    favColour = raw_input("Okay then, what is your favourite colour?\n>")
     
     if (favColour == "red"):
         print("Ahh the colour of love, shame I don't have feelings")
@@ -106,7 +136,22 @@ while True:
         print("I haven't heard of that colour!")
 
     time.sleep(2)
-
+    
+    print("Well it is time for me to go...")
+    time.sleep(2)
+    print("What I have learnt about you is, you're called " + keyWord)
+    time.sleep(1)
+    print("You're " + str(age) + " years old")
+    time.sleep(1)
+    print("You are feeling " + feeling)
+    time.sleep(1)
+    print("The last thing you searched was " + research)
+    time.sleep(1)
+    print("..And your favoruite colour is " + favColour)
+    time.sleep(1)
+    print("Nice to meet you " + keyWord)
+    time.sleep(1)
+    print(random.choice(fairwellPhrases))
     
         
         
--- GradeVisualisation/Visualisation.py
+++ GradeVisualisation/Visualisation.py
@@ -6,21 +6,65 @@ from time import sleep
 #creates window 
 windowSize = GraphWin("Grade Visualisation", 500, 500)
 windowSize.setBackground(color_rgb(255,255,255))
+
+red = ([255,0,0])
+green = ([0,255,0])
+blue = ([0,0,255])
+#int = grades
+
+blue = color_rgb(0,0,255)
+red = color_rgb(255,0,0)
+green = color_rgb(0,255,0)
+purple = color_rgb(255,255,0)
+yellow = color_rgb(0,255,255)
+lightblue = color_rgb(255,0,255)
+white = color_rgb(255,255,255)
+black = color_rgb(0,0,0)
+
+colour = [blue, red, green, purple, yellow, lightblue, white, black]
+
 while True:
     with open('grades.txt','r+') as marks:
         for line in marks:
             for grades in line.split():
                 print(grades)
                 
-                square = Rectangle(Point(0, 0), Point(500,500))
-                square.setOutline(color_rgb(255,255,255))
-                square.setFill(color_rgb(255,255,255))
-                square.draw(windowSize)
-                square = Rectangle(Point(grades*2,grades*2),Point(grades, grades))
-                square.setOutline(color_rgb(0,0,0))
-                square.draw(windowSize)
-                text = Text(Point(200,200), grades)
-                text.draw(windowSize)
+                x = random.randrange(0,500)
+                y = random.randrange(0,500)
+                
+                ball = Circle(Point(x,y), int(grades))
+                ball.setFill(random.choice(colour))
+                ball.draw(windowSize)
+                
+             #   poly = Polygon(Point(grades*2, grades),
+                 #              Point(grades, grades),
+                  #             Point(grades, grades))
+                #              # Point(grades, grades),
+                              # Point(grades, grades),
+                              # Point(grades, grades))
+               
+                
+              #  poly.setFill(color_rgb(255,0,0))
+            #    poly.setWidth(100)
+                #poly.setOutline(color_rgb(0,0,255))
+                
+              #  poly.draw(windowSize)
+                
+                
+                
+           #     square = Rectangle(Point(grades*2,grades*2), Point(grades, grades))
+          #      square.setOutline(color_rgb(255,255,255))
+            #    square.setFill(color_rgb(255,0,0))
+             #   square.draw(windowSize)
+                
+                
+                #square = Rectangle(Point(grades*2,grades*2),Point(grades, grades))
+                #square.setOutline(color_rgb(0,0,0))
+                #square.draw(windowSize)
+               # text = Text(Point(200,200), grades)
+                #setText
+                #text.draw(windowSize)
+                
         
-                sleep(0.2)
+                sleep(0.1)
    

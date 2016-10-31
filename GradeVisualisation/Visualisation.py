27c14d1f7983a39d7da00072037d881c16c67433
Glenn Winter <winter.glenn@hotmail.co.uk>
2016-10-31 06:34:28 +0000

    Final Visualisation

--- GradeVisualisation/Visualisation.py
+++ GradeVisualisation/Visualisation.py
@@ -2,69 +2,57 @@ from graphics import *
 import random
 from time import sleep
 
-
 #creates window 
 windowSize = GraphWin("Grade Visualisation", 500, 500)
 windowSize.setBackground(color_rgb(255,255,255))
 
-red = ([255,0,0])
-green = ([0,255,0])
-blue = ([0,0,255])
-#int = grades
+#makes a red circle
+def red():
+  circle = Circle(Point(x,y), int(grades))
+  circle.setFill(color_rgb(255,0,0))
+  circle.draw(windowSize)
+                    
+  gradeText = Text(Point(x,y), grades)
+  gradeText.draw(windowSize)
 
-blue = color_rgb(0,0,255)
-red = color_rgb(255,0,0)
-green = color_rgb(0,255,0)
-purple = color_rgb(255,255,0)
-yellow = color_rgb(0,255,255)
-lightblue = color_rgb(255,0,255)
-white = color_rgb(255,255,255)
-black = color_rgb(0,0,0)
+#makes a green circle  
+def green():
+  circle = Circle(Point(x,y), int(grades))
+  circle.setFill(color_rgb(0,255,0))
+  circle.draw(windowSize)
+                    
+  gradeText = Text(Point(x,y), grades)
+  gradeText.draw(windowSize)
 
-colour = [blue, red, green, purple, yellow, lightblue, white, black]
+#makes an orange circle
+def orange():
+  circle = Circle(Point(x,y), int(grades))
+  circle.setFill(color_rgb(255,165,0))
+  circle.draw(windowSize)
+                    
+  gradeText = Text(Point(x,y), grades)
+  gradeText.draw(windowSize)
 
+#Opens text file and reads grades in
 while True:
     with open('grades.txt','r+') as marks:
         for line in marks:
             for grades in line.split():
                 print(grades)
-                
+
+                #Places circle randomly in the window
                 x = random.randrange(0,500)
                 y = random.randrange(0,500)
+
+                #Depending on grade, different colour circles representing good or bad grades
+                if int(grades) >= int(60):
+                    green()
+
+                elif int(grades) >= int(50):
+                    orange()
                 
-                ball = Circle(Point(x,y), int(grades))
-                ball.setFill(random.choice(colour))
-                ball.draw(windowSize)
+                elif int(grades) <= int(49):
+                    red()    
                 
-             #   poly = Polygon(Point(grades*2, grades),
-                 #              Point(grades, grades),
-                  #             Point(grades, grades))
-                #              # Point(grades, grades),
-                              # Point(grades, grades),
-                              # Point(grades, grades))
-               
-                
-              #  poly.setFill(color_rgb(255,0,0))
-            #    poly.setWidth(100)
-                #poly.setOutline(color_rgb(0,0,255))
-                
-              #  poly.draw(windowSize)
-                
-                
-                
-           #     square = Rectangle(Point(grades*2,grades*2), Point(grades, grades))
-          #      square.setOutline(color_rgb(255,255,255))
-            #    square.setFill(color_rgb(255,0,0))
-             #   square.draw(windowSize)
-                
-                
-                #square = Rectangle(Point(grades*2,grades*2),Point(grades, grades))
-                #square.setOutline(color_rgb(0,0,0))
-                #square.draw(windowSize)
-               # text = Text(Point(200,200), grades)
-                #setText
-                #text.draw(windowSize)
-                
-        
                 sleep(0.1)
    
